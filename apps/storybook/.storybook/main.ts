5c5614d8a9ba1fafa6a0faae5883f326d3f6367a
Ben Henning <benhenning10@gmail.com>
2024-10-30 15:53:33 +0200

    stories functional

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,4 +1,5 @@
 import { dirname, join } from "path";
+import { StorybookConfig } from "@storybook/react-vite";
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -9,7 +10,7 @@ function getAbsolutePath(value) {
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+const config: StorybookConfig = {
   stories: [
     "../../../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../../../packages/ui/src/*.stories.@(js|jsx|mjs|ts|tsx)",
@@ -17,13 +18,13 @@ const config = {
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-themes"),
     getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
+  framework: "@storybook/react-vite",
+  docs: {
+    autodocs: true,
   },
   core: {
     disableTelemetry: true,
