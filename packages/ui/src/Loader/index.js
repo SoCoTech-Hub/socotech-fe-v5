4a91f62e3d511b139d2aa8ab41d5aa09234f2bcd
Ben Henning <benhenning10@gmail.com>
2024-11-06 13:03:00 +0200

    sort a couple components

--- packages/ui/src/Loader/index.js
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-	return (
-		<div className='p-4 mb-24 rounded-lg bg-compBg shadow-menu '>
-			<div className='h-16 my-3 bg-gray-700 rounded-md animate-pulse' />
-		</div>
-	)
-}
-
-export default Loader
